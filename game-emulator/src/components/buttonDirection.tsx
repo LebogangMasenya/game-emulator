f632fda14edf7e016b9d025ae9acba20a705be09
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-19 15:12:21 +0200

    React syntax fix

--- game-emulator/src/components/buttonDirection.tsx
+++ game-emulator/src/components/buttonDirection.tsx
@@ -1,4 +1,4 @@
-export default function createButtonDirection({pressedButtons, setPressedButtons}: {pressedButtons: string[], setPressedButtons: React.Dispatch<React.SetStateAction<string[]>>}) {
+export default function CreateButtonDirection({pressedButtons, setPressedButtons}: {pressedButtons: string[], setPressedButtons: React.Dispatch<React.SetStateAction<string[]>>}) {
     return (
         <div className="button-direction">
             <button className="up" onClick={() => setPressedButtons(prev => [...prev, "UP"])}>up</button>
@@ -6,5 +6,5 @@ export default function createButtonDirection({pressedButtons, setPressedButtons
             <button className="down" onClick={() => setPressedButtons(prev => [...prev, "DOWN"])}>down</button>
             <button className="left" onClick={() => setPressedButtons(prev => [...prev, "LEFT"])}>left</button>
         </div>
-    )
-}
+    );
+};
