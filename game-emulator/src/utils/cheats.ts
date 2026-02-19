0f4a7f26bc3db29946ab56d269ecb9ceb14aff77
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-19 21:47:29 +0200

    Separation of concerns of data

--- game-emulator/src/utils/cheats.ts
+++ game-emulator/src/utils/cheats.ts
@@ -1,23 +1,5 @@
 // import cheat codes
-function getCheatCodes() {
-  const cheatCodes = [
-    {
-      name: 'GODMODE',
-      sequence: ['UP', 'UP', 'DOWN', 'DOWN', 'LEFT', 'RIGHT', 'LEFT', 'RIGHT', 'B', 'A']
-    },
-    {
-      name: 'INFINITE_AMMO',
-      sequence: ['UP', 'UP', 'DOWN', 'DOWN', 'LEFT', 'RIGHT', 'LEFT', 'RIGHT', 'B', 'B']
-    },
-    {
-      name: 'UNLOCK_ALL_LEVELS',
-      sequence: ['UP', 'UP', 'DOWN', 'DOWN', 'LEFT', 'RIGHT', 'LEFT', 'RIGHT', 'A', 'A']
-    }
-  ]
-
-  return cheatCodes;
-
-}
+import getCheatCodes from "./data";
 
 export function checkCheatCode(pressedButtons: string[]) {
   const cheatCodes = getCheatCodes();
