6c683c0d26f6d19701c4079e519851ecfffe1531
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-19 21:47:29 +0200

    Separation of concerns of data

---
+++ game-emulator/src/utils/data.ts
@@ -0,0 +1,17 @@
+export default function getCheatCodes() {
+  const cheatCodes = [
+    {
+      name: 'GODMODE',
+      sequence: ['UP', 'UP', 'DOWN', 'DOWN', 'LEFT', 'RIGHT', 'LEFT', 'RIGHT', 'B', 'A']
+    },
+    {
+      name: 'INFINITE_AMMO',
+      sequence: ['UP', 'UP', 'DOWN', 'DOWN', 'LEFT', 'RIGHT', 'LEFT', 'RIGHT', 'B', 'B']
+    },
+    {
+      name: 'UNLOCK_ALL_LEVELS',
+      sequence: ['UP', 'UP', 'DOWN', 'DOWN', 'LEFT', 'RIGHT', 'LEFT', 'RIGHT', 'A', 'A']
+    }
+  ]
+  return cheatCodes;
+}
