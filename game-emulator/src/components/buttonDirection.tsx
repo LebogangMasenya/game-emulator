2ae7304aa938911667ecf1bc46190dd607d3ebff
Oarabile Mbewe <oarabilembewe@DVTLUNLVCH.local>
2026-02-19 15:07:23 +0200

    Working code create butto function aligned tp project scope

--- game-emulator/src/components/buttonDirection.tsx
+++ game-emulator/src/components/buttonDirection.tsx
@@ -1,12 +1,10 @@
-
-
 export default function createButtonDirection({pressedButtons, setPressedButtons}: {pressedButtons: string[], setPressedButtons: React.Dispatch<React.SetStateAction<string[]>>}) {
     return (
-         <div className="button-direction">
-            <button className="up" onClick={() => console.log("up")}>up</button>
-            <button className="right" onClick={() => console.log("right")}>right</button>
-            <button className="down" onClick={() => console.log("down")}>down</button>
-            <button className="left" onClick={() => console.log("left")}>left</button>
+        <div className="button-direction">
+            <button className="up" onClick={() => setPressedButtons(prev => [...prev, "UP"])}>up</button>
+            <button className="right" onClick={() => setPressedButtons(prev => [...prev, "RIGHT"])}>right</button>
+            <button className="down" onClick={() => setPressedButtons(prev => [...prev, "DOWN"])}>down</button>
+            <button className="left" onClick={() => setPressedButtons(prev => [...prev, "LEFT"])}>left</button>
         </div>
     )
 }
